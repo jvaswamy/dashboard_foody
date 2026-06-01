7dd0b8255f4f2f9ff5147537563d0bc099b90ec9
jvaswamy <109134449+jvaswamy@users.noreply.github.com>
2026-06-01 15:41:30 +0530

    modify some files (#1)
    Co-authored-by: jvaswamy <jvaswamy@gmail.com>

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { assets } from "../../public/assets/assets";
 
 const Welcome = () => {
   return (
     <div className="welcome-container">
-      <img src="/assets/chef.jpg" alt="Welcome" className="welcome-image" />
+      <img src={assets.chef} alt="Welcome" className="welcome-image" />
       <h1>Welcome to Our Website</h1>
       <p>Your journey to explore begins here.</p>
     </div>
